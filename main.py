fb6358ab8077819a4d0a5e0b96f3a3d064d48407
dayna_l <dayna.lashin@gmail.com>
2021-03-16 00:28:26 -0400

    Started building out breed selector and set up route to pull images from API

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, requests
+
+import requests
+
+#imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
+from dog_breeds import prettify_dog_breed
 
 app = Flask("app")
 
-@app.route("/")
-def hello_world():
+#function adds a dash in the URL between breed names with multiple words like miniature poodle
+def check_breed(breed):
+  return "/".join(breed.split("-"))
+
+@app.route("/", methods=["GET", "POST"])
+def dog_image_gallery():
+  errors = []
+  if request.method == "POST":
+    breed = request.form.get("breed")
+
+    if not breed:
+      errors.append("Oops! Please choose a breed")
+
+    if breed:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
+      data = response.json()
+      dog_images = data["message"]
+
   return render_template("dogs.html")
 
+
+app.debug = True
 app.run(host='0.0.0.0', port=8080)
